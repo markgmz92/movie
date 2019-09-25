867b96eda48b3136c25da36646350f2690048879
markgmz92 <markgmz92@gmail.com>
2019-09-25 18:04:07 +0800

    Created table view for movie row

--- src/App.js
+++ src/App.js
@@ -2,6 +2,42 @@ import React, { Component } from "react";
 import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    console.log("This is my initalizer");
+
+    const movies = [
+      { id: 0, title: "Avengers: Infinity War", overview: "Bla bla bla bla" },
+      { id: 1, title: "Avengers", overview: "bla bla bla bla" }
+    ];
+
+    this.state = {
+      rows: [
+        <p key="1">This is my row 0</p>,
+        <p key="2">This is my row 1</p>,
+        <p key="3">This is my row 2</p>
+      ]
+    };
+
+    var movieRows = [];
+    movies.forEach(movie => {
+      console.log(movie.title);
+      const movieRow = (
+        <table key={movie.id}>
+          <tr>
+            <td>
+              <img alt="" src="" />
+            </td>
+            <td>{movie.title}</td>
+          </tr>
+        </table>
+      );
+
+      movieRows.push(movieRow);
+    });
+
+    this.state = { rows: movieRows };
+  }
   render() {
     return (
       <div className="App">
@@ -30,6 +66,8 @@ class App extends Component {
           }}
           placeholder="Enter search term"
         />
+
+        {this.state.rows}
       </div>
     );
   }
